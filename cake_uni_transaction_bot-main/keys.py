803416c818bd13961a03aaef0b48f9ae37ec7354
hongquan <hongquan_tq92@yahoo.com>
2021-11-24 18:30:50 +0700

    # features add actions

---
+++ cake_uni_transaction_bot-main/keys.py
@@ -0,0 +1,6 @@
+#Infura node
+infura_project_id = ""
+
+# Metamask keys
+metamask_address = "0xF5eF1ECF1508Fd5D16Bc995E712f0020eb0e90A1"
+metamask_private_key = "739c45243e8702e2ffce4f62d7af270a0abfa7e3af81b4906e05f31ff1c16231"
